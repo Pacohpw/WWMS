a4bfba29470cb7e2c71d391584a5b47733b417f7
Pacohpw <pacoho20031106@gmail.com>
2025-05-10 20:59:39 +0800

    Update scriptLoginRegister.js

--- frontend/scriptLoginRegister.js
+++ frontend/scriptLoginRegister.js
@@ -52,7 +52,7 @@ document.getElementById('registerForm').addEventListener('submit', async functio
     }
 
     try {
-        const response = await fetch('http://localhost:5000/api/auth/register', {
+        const response = await fetch('https://wwms-backend.onrender.com/api/auth/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
