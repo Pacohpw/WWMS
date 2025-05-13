a5d1ce144045bcff72fe09db64b99fa33f19bb96
Pacohpw <pacoho20031106@gmail.com>
2025-05-13 14:54:39 +0800

    Update jwt.js

--- backend/src/services/jwt.js
+++ backend/src/services/jwt.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const generateToken = (user) => {
   return jwt.sign(
-    { id: user.id, role: user.role, countryid: user.countryid },
+    { id: user.id, role: user.role, countryid: user.countryid, username: user.username},
     process.env.JWT_SECRET,
     { expiresIn: '1h' }
   );
