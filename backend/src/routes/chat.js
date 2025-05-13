2e8444f8c98e494b830c4f647199acd27b57a212
Pacohpw <pacoho20031106@gmail.com>
2025-05-13 17:19:04 +0800

    Update chat.js

--- backend/src/routes/chat.js
+++ backend/src/routes/chat.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const authenticateToken = require('../middlewares/auth');
 const Message = require('../models/Message'); 
 const User = require('../models/User'); 
 
